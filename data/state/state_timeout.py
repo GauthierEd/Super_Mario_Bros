86bd1897df1e084e1f2014c1ec15293b3825bae4
HarlausG <73186172+HarlausG@users.noreply.github.com>
2020-12-13 16:41:32 +0100

    Updated TimeOut State
    State become the child of State Class from state.py

--- data/state/state_timeout.py
+++ data/state/state_timeout.py
@@ -1,12 +1,12 @@
 import pygame as pg
 from .. components import info
 from .. import constante as c 
+from . import state
 
-class timeOut(object):
+class timeOut(state.State):
     def __init__(self):
-        self.done = False
-        self.next = c.LOAD
-        self.current_update = 0
+        state.State.__init__(self)
+
         
     def startup(self,current_time):
         if info.game_info["lives"] == 0:
@@ -16,13 +16,14 @@ class timeOut(object):
         self.info = info.Info(c.TIMEOUT)
         self.current_update = current_time
         
-    def cleanup(self):
-        self.done = False
     
+    def draw_everything(self,screen):
+        screen.fill((0,0,0))
+        self.info.draw(screen)
+
     def update(self,keys,screen,current_time):
         if current_time - self.current_update < 3000:
             self.info.update(current_time)
-            screen.fill((0,0,0))
-            self.info.draw(screen)
+            self.draw_everything(screen)
         else:
             self.done = True
